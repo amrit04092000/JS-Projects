f9f4b93cfcf9823b942588b72a75f167048cee0f
amrit04092000 <amrit04092000@gmail.com>
2021-08-02 15:08:50 +0530

    trash button alignment prob

--- script.js
+++ script.js
@@ -1,16 +1,18 @@
-document.querySelector('#push').onClick = function () {
+document.querySelector('#push').onclick = function () {
     console.log('Button clicked');
-//   if (document.querySelector('#newTask input').value.length == 0) {
-//     alert("Please enter Task");
-//   } 
-//   else {
-//     document.querySelector('#tasks').innerHTML += `
-//                 <div class="task">
-//                     <span id="taskname">
-//                         ${document.querySelector('#newTask input').value}
-//                     </span>
-
-//                 <div>
-//         `;
-//   }
+  if (document.querySelector('#newTask input').value.length == 0) {
+    alert("Please enter Task");
+  } 
+  else {
+    document.querySelector('#tasks').innerHTML += `
+                <div class="task">
+                    <span id="taskname">
+                        ${document.querySelector('#newTask input').value}
+                    </span>
+                    <button class="delete">
+                        <i class="far fa-trash-alt"></i>
+                    </button>
+                <div>
+        `;
+  }
 };
