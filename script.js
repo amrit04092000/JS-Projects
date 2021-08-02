0578f9e59357a748317daa4dcdf5f5bc98cf5071
amrit04092000 <amrit04092000@gmail.com>
2021-08-02 13:49:41 +0530

    script not work

---
+++ script.js
@@ -0,0 +1,16 @@
+document.querySelector('#push').onClick = function () {
+    console.log('Button clicked');
+//   if (document.querySelector('#newTask input').value.length == 0) {
+//     alert("Please enter Task");
+//   } 
+//   else {
+//     document.querySelector('#tasks').innerHTML += `
+//                 <div class="task">
+//                     <span id="taskname">
+//                         ${document.querySelector('#newTask input').value}
+//                     </span>
+
+//                 <div>
+//         `;
+//   }
+};
